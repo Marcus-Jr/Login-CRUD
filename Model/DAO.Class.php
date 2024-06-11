a03940f2c0c28b4e8b75e997655be6020e4086db
Marcus <133683002+ForgeSolucoesEstagio@users.noreply.github.com>
2024-06-11 11:55:26 -0300

    Functions

--- Model/DAO.Class.php
+++ Model/DAO.Class.php
@@ -1,16 +1,65 @@
 <?php
-function conexao()
+class DAO
 {
-    $hostname = "localhost";
-    $bancodedados = "alunos";
-    $usuario = "root";
-    $senha = "";
+    private $NomeDB;
+    private $Senha;
+
+    public function Conexao()
+    {
+        $hostname = "localhost";
+        $bancodedados = "alunos";
+        $usuario = "root";
+        $senha = "";
 
 
-    $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
-    if ($mysqli->connect_errno) {
-        echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    } else {
-        echo "Conectado!";
+        $conexao = new mysqli($hostname, $usuario, $senha, $bancodedados);
+        if ($conexao->connect_errno) {
+            echo "Falha ao conectar: (" . $conexao->connect_errno . ") " . $conexao->connect_error;
+        } else {
+            echo "Conectado!";
+        }
+        return $conexao;
+    }
+
+    public function SelecionaIdAluno($dadosArray)
+    {
+
+        $conexao = $this->Conexao();
+
+        $tabela = 'nome da tabela';
+        $coluna = 'nome da coluna';
+        $id = $dadosArray;
+        $salvar = $conexao->prepare("select * from $tabela where $coluna = '$id'");
+        $salvar->execute();
+
+        $resultado = $salvar->fetch(PDO::FETCH_ASSOC);
+        echo json_encode($resultado);
+    }
+
+    public function InserirNoBanco($dadosArray)
+    {
+
+        $conexao = $this->Conexao();
+
+        $tabela = 'nome da tabela';
+        $nome = $dadosArray["nome"];
+        $nascimento = $dadosArray["nascimento"];
+        $turma = $dadosArray['turma'];
+        $nacionalidade = $dadosArray['nac'];
+        $endereco = $dadosArray['end'];
+
+        $salvar = $conexao->prepare("insert into $tabela values (default,'$nome','$nascimento','$turma','$nacionalidade','$endereco')");
+        $salvar->execute();
+    }
+
+    public function ListarAlunos(){
+        
+        $conexao = $this->conexao();
+
+        $tabela = 'nome da tabela';
+        $salvar = $conexao->prepare("select * from $tabela");
+        $salvar->execute();
+        $resultado = $salvar->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($resultado);
     }
 }
