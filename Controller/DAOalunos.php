d9018e195e87f6d04245f341b60772f31d4d2da6
ForgeSolucoesEstagio <>
2025-05-31 18:30:13 -0300

    Controller
    Arquivos controladores principais

--- Controller/DAOalunos.php
+++ Controller/DAOalunos.php
@@ -4,20 +4,54 @@ include_once '../Model/Alunos.Class.php';
 class DadosController extends Alunos
 {
 
-    public function InserirAluno($coisa)
+    public function InserirAluno($dados)
     {
-        $this->InserirNoBanco($coisa);
+        $aluno = [
+            Alunos::NOME          => $dados->nome,
+            Alunos::NASCIMENTO    => $dados->nascimento,
+            Alunos::TURMA         => $dados->turma,
+            Alunos::NACIONALIDADE => $dados->nacionalidade,
+            Alunos::ENDERECO      => $dados->endereco
+        ];
+        $this->Inserir(Alunos::TABELA, $aluno);
     }
-    public function Listar($dadosArray)
+
+    public function ListarAlunos()
     {
-        $this->ListarAlunos($dadosArray);
+        $this->Listar(Alunos::TABELA);
+    }
+
+    public function BuscarAluno($dados)
+    {
+        $condicao = [Alunos::ID => $dados->id];
+        $this->Buscar(Alunos::TABELA, $condicao);
+    }
+
+    public function ExcluirAluno($dados)
+    {
+        $condicao = [Alunos::ID => $dados->id];
+        $this->Excluir(Alunos::TABELA, $condicao);
+    }
+
+    public function EditarAluno($dados)
+    {
+        $aluno = [
+            Alunos::NOME          => $dados->nome,
+            Alunos::NASCIMENTO    => $dados->nascimento,
+            Alunos::TURMA         => $dados->turma,
+            Alunos::NACIONALIDADE => $dados->nacionalidade,
+            Alunos::ENDERECO      => $dados->endereco
+        ];
+        $condicao = [Alunos::ID => $dados->id];
+        
+        $this->Editar(Alunos::TABELA, $aluno, $condicao);
     }
 }
 
 $post = file_get_contents('php://input');
 $ObjetoDados = json_decode($post);
 $dadosArray = get_object_vars($ObjetoDados);
-$DadosParaFuncao = get_object_vars($dadosArray['dados']);
+$DadosParaFuncao = $dadosArray['dados'] ? $dadosArray['dados'] : '';
 $funcao = $dadosArray['funcao'];
 $ObjAlunos = new DadosController;
 $ObjAlunos->$funcao($DadosParaFuncao);
--- Controller/DAOlogin.php
+++ Controller/DAOlogin.php
@@ -1,50 +1,52 @@
 <?php
-include '../Model/DAO.Class.php';
+include '../Model/Login.Class.php';
 
-class Login extends DAO {
+class LoginController extends Login 
+{
+    public function InserirAcesso($dados){
 
-private $login;
-private $senha;
-
-
-    public function FazLogin($dadosArray){
-        $this->Login($dadosArray);
+        $login = [
+            Login::USUARIO => $dados->nome,
+            Login::EMAIL => $dados->email,
+            Login::SENHA => password_hash($dados->senha, PASSWORD_DEFAULT),
+            Login::DATA_CADASTRO => date("Y-m-d"),
+            Login::DATA_ULTIMO_LOGIN => date("Y-m-d")
+        ];
+        return $this->Inserir(Login::TABELA, $login);
     }
 
-    public function getLogin()
-    {
-        return $this->login;
+    public function Logar($dados){
+        
+        $user = $this->BuscaUsuario($dados);
+
+        if(!empty($user)){
+            if(password_verify($dados->senha, $user['SENHA'])) {
+                $retorno = ['success' => true, 'message' => 'Bem vindo,' . $user['USUARIO']];
+            } else {
+                $retorno = ['success' => false, 'message' => 'Dados incorretos!'];
+            }
+        } else {
+            $retorno = ['success' => false, 'message' => 'Dados incorretos!'];
+        }
+        
+        return json_encode($retorno);
     }
 
+    public function BuscaUsuario($dados){
 
-    public function setLogin($login): self
-    {
-        $this->login = $login;
+        $condicao = [Login::EMAIL => $dados->email];
+        $campos   = [Login::SENHA , Login::USUARIO, Login::EMAIL];
+        return $this->BuscarCampos(Login::TABELA, $condicao, $campos);
 
-        return $this;
-    }
-
-
-    public function getSenha()
-    {
-        return $this->senha;
-    }
-
-
-    public function setSenha($senha): self
-    {
-        $this->senha = $senha;
-
-        return $this;
     }
 }
 
 $post = file_get_contents('php://input');
-$objetoJson= json_decode($post);
-$dadosArray = get_object_vars($objetoJson);
-$DadosParaFuncao = get_object_vars($dadosArray['dados']);
+$ObjetoDados = json_decode($post);
+$dadosArray = get_object_vars($ObjetoDados);
+$DadosParaFuncao = $dadosArray['dados'] ? $dadosArray['dados'] : '';
 $funcao = $dadosArray['funcao'];
-$ObjLogin = new Login;
+$ObjLogin = new LoginController;
 $ObjLogin->$funcao($DadosParaFuncao);
 
 ?>
